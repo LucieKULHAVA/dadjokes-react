ffd4aadfe82bf595c251bd605d61e1a4b4670f11
LucieKULHAVA <46672152+LucieKULHAVA@users.noreply.github.com>
2021-06-04 07:11:58 +0200

    Update index.jsx

--- src/Joke/index.jsx
+++ src/Joke/index.jsx
@@ -6,7 +6,7 @@ const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
   const [usersDislikes, setUserDislikes] = useState(dislikes);
 
   return (
-    <div className="joke">
+   <div style={{ opacity: usersDislikes >usersLikes ? '50%' : '100%' }} className="joke">
       <div className="joke__body">
         <div className="joke__user">
           <img className="user-avatar" src={userAvatar} />
@@ -15,14 +15,14 @@ const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
         <p className="joke__text">{text}</p>
       </div>
       <div className="joke__likes">
-        <button id="btn-up" className="btn-like btn-like--up"  
+        <button id="btn-up" value={usersLikes} className="btn-like btn-like--up"  
         onClick=
         {() => setUserLikes((palec) => palec + 1)}
         ></button>
         <span id="likes-up" className="likes-count likes-count--up">
           {usersLikes}
         </span>
-        <button id="btn-down" className="btn-like btn-like--down" 
+        <button id="btn-down" value={usersDislikes} className="btn-like btn-like--down" 
         onClick=
         {() => setUserDislikes((palec) => palec + 1)}
         ></button>
